10ac36bb1663fb8fb6a60e128942d4360a4784c7
bcdavis1990 <bcdavis1990@yahoo.com>
2022-01-22 08:25:06 -0500

    Code commenting done
    Added final code commenting before first review

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
 
 const buttonElement = document.getElementById("btn__reset");
 let game;
+
+//starts new game and resets elements back to default value
 buttonElement.addEventListener("click", (e) => {
   game = new Game();
   game.reset(e);
@@ -13,6 +15,7 @@ buttonElement.addEventListener("click", (e) => {
 const button = document.getElementsByClassName("key");
 
 for (let i = 0; i < button.length; i++) {
+  const button = document.getElementsByClassName("key");
   button[i].addEventListener("click", (event) => {
     game.handleInteraction(event.target);
   });
